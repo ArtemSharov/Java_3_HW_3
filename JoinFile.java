d9aa4dc0d02ae0a691910ae4fed07d166da6ed5a
Artem Sharov <sharov.a.p.10@gmail.com>
2020-04-12 12:12:40 +0300

    HW complete

--- JoinFile.java
+++ JoinFile.java
@@ -1,4 +1,29 @@
 package ru.gb.java_3.hw_3;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+//Последовательно сшить 5 файлов в один (файлы примерно 100 байт).
+// Может пригодиться следующая конструкция: ArrayList<InputStream> al = new ArrayList<>();
+// ... Enumeration<InputStream> e = Collections.enumeration(al);
 public class JoinFile {
+    public static void main(String[] args) throws IOException {
+        File file_1 = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example1.txt");
+        File file_2 = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example2.txt");
+        File file_3 = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example3.txt");
+        File file_4 = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example4.txt");
+        File file_5 = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example5.txt");
+        ArrayList<FileInputStream> arrFile = new ArrayList<>();
+        arrFile.add(new FileInputStream(file_1));
+        arrFile.add(new FileInputStream(file_2));
+        arrFile.add(new FileInputStream(file_3));
+        arrFile.add(new FileInputStream(file_4));
+        arrFile.add(new FileInputStream(file_5));
+        SequenceInputStream in = new SequenceInputStream(Collections.enumeration(arrFile));
+        int x;
+        while((x = in.read()) != -1) {
+            System.out.print((char)x);
+        }
+    }
+
 }
--- ReadFile.java
+++ ReadFile.java
@@ -1,4 +1,31 @@
 package ru.gb.java_3.hw_3;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+//Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
 public class ReadFile {
+
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Sharov\\IdeaProjects\\Java_3\\src\\ru\\gb\\java_3\\hw_3\\example1.txt");
+
+        byte[] buf = new byte[50];
+
+        try (FileInputStream in = new FileInputStream(file)) {
+            int count;
+            while ((count = in.read(buf)) > 0) {
+                for (int i = 0; i < count; i++) {
+                    System.out.print((char) buf[i]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
